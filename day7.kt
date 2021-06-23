e6998af0e41eb8b4de406881632f5031a0d27e45
“kkiirr201314” <qq255073@gmail.com>
2021-06-23 13:45:13 +0800

    c7-3

--- day7.kt
+++ day7.kt
@@ -8,4 +8,10 @@ private  fun placeOrder(menuData: String){
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfAppstrophe)
     println("Madrigal對 $tavernMaster 說了他們訂的酒水。 ")
     println(menuData)
+    val data = menuData.split(',')
+    val type = data[0]
+    val name = data[1]
+    val price = data[2]
+    val message = "Madrigal 買了一杯 $name ($type) 花了 $price."
+    println(message)
 }

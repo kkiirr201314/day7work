9b644e3e9f7ad08c933abb68a0542c802791e42d
“kkiirr201314” <qq255073@gmail.com>
2021-06-23 14:18:36 +0800

    c7-6

--- day7.kt
+++ day7.kt
@@ -1,6 +1,6 @@
 const val  TAVERN_NAME = "Taerny's Folly"
 fun main(args: Array<String>) {
-    placeOrder("shandy , Dragon's Breath, 5.91")
+    placeOrder("shandy ,Dragon's Breath, 5.91")
 }
 
 private fun toDragonSpeak(phrase: String) =
@@ -19,7 +19,7 @@ private  fun placeOrder(menuData: String){
     val indexOfAppstrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfAppstrophe)
     println("Madrigal對 $tavernMaster 說了他們訂的酒水。 ")
-    println(menuData)
+  //  println(menuData)
   //  val data = menuData.split(',')
   //  val type = data[0]
   //  val name = data[1]
@@ -28,6 +28,12 @@ private  fun placeOrder(menuData: String){
     val message = "Madrigal 買了一杯 $name ($type) 花了 $price."
     println(message)
 
-    val phrase = "Ah, dlicious $name!"
-    println("Madrigal 驚呼道: ${toDragonSpeak(phrase)}")
+  //  val phrase = "Ah, dlicious $name!"
+  //  println("Madrigal 驚呼道: ${toDragonSpeak(phrase)}")
+    val phrase = if (name == "Dragon's Breath"){
+        "Madrigal exclaims ${toDragonSpeak("Ah, dlicious $name!")}"
+    } else {
+        "Madrigal 說道: 感謝 $name ."
+    }
+    println(phrase)
 }
